40937c3fb0eb5fcb706af50368672804aaad85c3
MInealex <alejandrosanchezpinto@gmail.com>
2023-10-13 17:23:31 +0200

    Ejercicio terminado

--- python-3/ejercicio3.py
+++ python-3/ejercicio3.py
@@ -57,24 +57,59 @@ def ejercicio_3():
             "Pesto Rojo": 3,
             "Boloñesa": 2.5
         },
-        "IngredientesPizza": {
-            "Extra de queso": 1,
-            "Queso azul": 2,
-            "Salchicha": 1.5,
-            "Carne": 2,
-            "Chorizo": 2.5,
-            "Jamón": 3,
-            "Pimiento Verde": 1,
-            "Aceitunas": 1.5,
-            "Anchoas": 3,
-            "Atún": 2.5,
-            "Salmón": 3,
-            "Piña": 1.5
-        },
         "Postres": {
             "Tiramisú": 6,
             "Panna Cotta": 5.5,
         }
     }
+    IngredientesPizza = {
+        "Extra de queso": 1,
+        "Queso azul": 2,
+        "Salchicha": 1.5,
+        "Carne": 2,
+        "Chorizo": 2.5,
+        "Jamón": 3,
+        "Pimiento Verde": 1,
+        "Aceitunas": 1.5,
+        "Anchoas": 3,
+        "Atún": 2.5,
+        "Salmón": 3,
+        "Piña": 1.5
+    }
+
+    precio = 0
+
+    if input("¿Quieres pasta? ") == "si":
+        print(platos["Pasta"])
+        pasta = input("¿Qué pasta quieres? ")
+        if pasta in platos["Pasta"]:
+            precio += platos["Pasta"][pasta]
+            if input("¿Quieres salsa? ") == "si":
+                print (platos["Salas"])
+                salsa = input("¿Qué salsa quieres? ")
+                if salsa in platos["Salas"]:
+                    precio += platos["Salas"][salsa]
+    if input("¿Quieres pizza? ") == "si":
+        print(platos["Pizza"])
+        pizza = input("¿Qué pizza quieres? ")
+        if pizza in platos["Pizza"]:
+            precio += platos["Pizza"][pizza]
+            for i in range(5):
+                print(IngredientesPizza)
+                ingrediente = input("¿Qué ingrediente quieres? ")
+                if ingrediente in IngredientesPizza:
+                    precio += IngredientesPizza[ingrediente]
+                if i < 4:
+                    if input("¿Quieres más ingredientes? ") == "no":
+                        break
+    if input("¿Quieres postre? ") == "si":
+        print(platos["Postres"])
+        postre = input("¿Qué postre quieres? ")
+        if postre in platos["Postres"]:
+            precio += platos["Postres"][postre]
+
+
+    print(f'El precio es {precio} €')
+ejercicio_3()
 
 
