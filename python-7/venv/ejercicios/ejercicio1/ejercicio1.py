a73bf1526ec6c98e4f567c851084282de65a700b
MInealex <alejandrosanchezpinto@gmail.com>
2023-10-30 16:32:41 +0100

    Git ignore solucionado

--- python-7/venv/ejercicios/ejercicio1/ejercicio1.py
+++
@@ -1,6 +0,0 @@
-try:
-    raw_input = input("Introduce un número: ")
-    x = int(raw_input)
-    print(f"El resultado es", x*x)
-except ValueError:
-    print("No es un número")
--- python-7/venv/ejercicios/ejercicio3/ejercicio3.py
+++
@@ -1,7 +0,0 @@
-try:
-    r = 34/0
-    x = 5 + "hola"
-except ZeroDivisionError:
-    print("No se puede dividir por cero")
-except TypeError:
-    print("No se puede sumar un entero con un string")
